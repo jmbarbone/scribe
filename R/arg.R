5b4b99e741b73bb7a6f9c4fc0ff5cafef6e8b13c
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-09-13 21:38:32 -0400

    rewrite args as ReferenceClass objects; move actions functions here

--- R/arg.R
+++ R/arg.R
@@ -1,24 +1,138 @@
 
-#' New argument
-#'
-#' Create a new argument class
-#'
-#' @param id An integer to represent the sequence of argument
-#' @param aliases One or more names to be associated with the argument
-#' @param action An action for the paramater
-#' @param default Default value
-#' @param
-#' @export
+# TODO Arg as ReferenceClass, too
+
 new_arg <- function(
     id,
     aliases = NULL,
     action = NULL,
+    options = NULL,
+    type = NULL,
     default = NULL,
     help = NULL,
     n = 0L
 ) {
+  Arg$new(
+    id      = id,
+    aliases = aliases,
+    action  = action,
+    options = options,
+    type    = type,
+    default = default,
+    help    = help,
+    n       = n
+  )
+}
 
-  if (is.null(aliases)) {
+# ReferenceClass ----------------------------------------------------------
+
+Arg <- setRefClass(
+  "scribeArg",
+  fields = list(
+    id = "integer",
+    aliases = "character",
+    action = "character",
+    options = "character",
+    type = "character",
+    default = "character",
+    help = "character",
+    choices = "list",
+    n = "integer",
+    mult = "logical"
+  )
+)
+
+Arg$methods(
+  initialize = function(
+    id = NULL,
+    aliases = NULL,
+    action = NULL,
+    options = NULL,
+    type = NULL,
+    default = NULL,
+    help = NULL,
+    n = NULL
+  ) {
+    # TODO include validation
+    arg_initialize(
+      .self,
+      id      = id,
+      aliases = aliases,
+      action  = action,
+      options = options,
+      type    = type,
+      default = default,
+      help    = help,
+      n       = n
+    )
+  },
+
+  show = function() {
+    arg_show(.self)
+  },
+
+  help = function() {
+    # should return a matrix
+    arg_help()
+  },
+
+  do_action = function(value = NULL) {
+    arg_do_action(.self, value = value)
+  },
+
+  get_aliases = function() {
+    arg_get_aliases(.self)
+  },
+
+  get_name = function() {
+    arg_get_name(.self)
+  },
+
+  get_action = function() {
+    arg_get_action(.self)
+  },
+
+  get_default = function() {
+    arg_get_default(.self)
+  },
+
+  match = function(commands) {
+    arg_match(.self)
+  }
+)
+
+# wrappers ----------------------------------------------------------------
+
+arg_initialize <- function(
+    self,
+    id,
+    aliases,
+    action,
+    type,
+    default,
+    options,
+    n,
+    help
+) {
+  action  <- action  %||% "none"
+  type    <- type    %||% "any"
+  default <- default %||% first(options) %||% ""
+  options <- options %||% ""
+  help    <- help    %||% ""
+  n       <- n       %||% 1L
+
+  stopifnot(
+    is_intish(id),
+    length(id) == 1L,
+    length(aliases) > 0L,
+    length(n) == 1L,
+    isTRUE(n) || (is_intish(n) & n >= 0L)
+  )
+
+  if (action == "command") {
+    if (any(grepl(ARG_PAT, alises, ignore.case = TRUE))) {
+      stop("command must ")
+    }
+  } else if (is.null(aliases)) {
     aliases <- "..."
   } else {
     aliases <- unlist(aliases, recursive = TRUE, use.names = FALSE)
@@ -29,32 +143,20 @@ new_arg <- function(
     )
   }
 
-  stopifnot(
-    is_intish(id),
-    length(id) == 1L,
-    length(aliases) > 0L,
-    length(n) == 1L,
-    isTRUE(n) || (is_intish(n) & n >= 0L)
-  )
-
-  structure(
-    as.integer(id),
-    class = "scribe_arg",
-    aliases = aliases,
-    # TODO consider validate_action()
-    action = action,
-    value = default %||% if (identical(action, "bool")) TRUE,
-    help = help,
-    n = as.integer(n)
-  )
+  self$id      <- as.integer(id)
+  self$aliases <- aliases
+  self$action  <- action
+  self$type    <- type %||% typeof(default) %||% "any"
+  self$default <- default
+  self$help    <- help
+  self$n       <- if (isTRUE(n)) 0L else n
+  self$mult    <- isTRUE(n)
+  self
 }
 
-ARG_PAT <- "^-[a-z]$|^--[a-z]+$|^--[a-z](+[-]?[a-z]+)+$"
-
-#' @export
-print.scribe_arg <- function(x, ...) {
+arg_show <- function(self) {
   # TODO add print_scribe.arg
-  value <- attr(x, "value")
+  value <- self$get_default()
   value <-
     if (is.null(value)) {
       "<null>"
@@ -62,15 +164,93 @@ print.scribe_arg <- function(x, ...) {
       paste(vapply(value, format, NA_character_), collapse = " ")
     }
 
-  aliases <- to_string(attr(x, "aliases"))
+  aliases <- to_string(self$get_aliases())
   print_line(sprintf("Argument [%s] : %s", aliases, value))
-  invisible(x)
+  invisible(self)
+}
+
+arg_help <- function(self) {
+  print_lines(
+    sprintf("[%s]", to_string(attr(x, "aliases")))
+  )
+}
+
+arg_do_action <- function(self, ca) {
+  # Pass the scribeCommandArg so we can update/remove
+  stopifnot(is_command_args(ca))
+
+  switch(
+    match.arg(arg$get_action()),
+    ## none ----
+    none = value %||% arg$get_default(),
+
+    ## flag ----
+    # if present, identify as yes/no.
+    # TODO account for --no-option
+    flag = {
+      found <- match(arg$get_aliases(), "")
+    },
+
+    command = {
+      match.arg(value, arg$get_options(), several.ok = FALSE)
+    }
+  )
+}
+
+action_validate <- function(action = c("none", "command", "bool", "flag")) {
+  match.arg(action %||% "none")
+}
+
+
+arg_get_aliases <- function(self) {
+  self$aliases
+}
+
+arg_get_name <- function(self) {
+  al <- self$get_aliases()
+  ind <- grep("^--", al)
+
+  if (length(ind)) {
+    al[ind[1L]]
+  } else {
+    al[1L]
+  }
+}
+
+arg_get_action  <- function(self) {
+  self$action %||% "none"
+}
+
+arg_get_default <- function(self) {
+  self$default
+}
+
+arg_match <- function(self, commands, n = 1L) {
+  m <- match(self$get_aliases(), commands, 0L)
+  m <- m[m > 0L]
+
+  if (!isTRUE(n) && length(m) > n) {
+    msg <- sprintf(
+      "%s matches %i times but should only match %i time(s)",
+      self$get_names(),
+      length(m),
+      n
+    )
+    stop(msg, call. = FALSE)
+  }
+
+  m
 }
 
 # helpers -----------------------------------------------------------------
 
-get_value <- function(x) {
-  stopifnot(inherits(x, "scribe_args"))
-  # TODO flesh out
-  attr(x, "value", exact = TRUE)
+is_arg <- function(x) {
+  identical(class9x)
+}
+
+ARG_PAT <- "^-[a-z]$|^--[a-z]+$|^--[a-z](+[-]?[a-z]+)+$"
+
+is_command <- function(x) {
+  stopifnot(is.list(x))
+  vapply(x, function(i) i$get_action(), NA_character_) == "command"
 }
