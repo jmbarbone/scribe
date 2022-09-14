16d31aeeb04c141e2cd4e52063f8c011420919c3
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-09-13 21:38:09 -0400

    include more utils

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,11 @@
 
-`%||%` <- function(x, y) { if (is.null(x)) y else x }
+# slightly different here
 
+`%||%` <- function(x, y) { if (is_empty(x)) y else x }
+
+is_empty <- function(x) {
+  is.null(x) || !nzchar(x) || length(x) == 0L
+}
 
 to_string <- function(..., sep = " ") {
   lines <- list(...)
@@ -8,8 +13,8 @@ to_string <- function(..., sep = " ") {
   paste0(lines, collapse = sep)
 }
 
-print_lines <- function(..., sep = "\n") {
-  writeLines(to_string(..., sep = sep))
+print_lines <- function(..., sep = "\n", to = stdout()) {
+  writeLines(to_string(..., sep = sep), con = to)
 }
 
 print_line <- function(...) {
@@ -22,4 +27,12 @@ print_line0 <- function(...) {
 
 is_intish <- function(x) {
   !is.null(x) && (is.numeric(x) | all(is.na(x))) && !isFALSE(x %% 1 == 0)
+}
+
+maybe_quit <- function(force = !interactive()) {
+  if (force) quit(save = "no", status = 0L) else invisible()
+}
+
+first <- function(x) {
+  if (length(x)) x[1L] else x[0L]
 }
