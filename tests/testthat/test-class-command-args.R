09985c69f24da0d648d658b8f7edfc71607e7958
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-11-28 10:26:15 -0500

    add tests for default values

--- tests/testthat/test-class-command-args.R
+++ tests/testthat/test-class-command-args.R
@@ -3,12 +3,29 @@ test_that("command_args() works", {
   ca <- command_args(x)
   ca$add_argument("-a", "--alpha", type = "integer")
   ca$add_argument("-b", "--beta", type = "integer")
-  # debugonce(ca_resolve_args)
-  # debug(arg_parse_value)
-  ca$resolve()
-  ca$get_values()
 
   obj <- ca$parse()
   exp <- list(alpha = 1L, beta = 2L)
+  expect_identical(obj, exp)
+})
+
+test_that("command_args() handles defaults", {
+  foo <- function(x = character()) {
+    ca <- command_args(x)
+    ca$add_argument("-a", "--alpha", default = 1L)
+    ca$add_argument("-b", "--beta",  default = 1L)
+    ca$parse()
+  }
+
+  obj <- foo()
+  exp <- list(alpha = 1L, beta = 1L)
+  expect_identical(obj, exp)
+
+  obj <- foo(c("-a", "0"))
+  exp <- list(alpha = 0L, beta = 1L)
+  expect_identical(obj, exp)
+
+  obj <- foo(c("-a", "2", "--beta", "3"))
+  exp <- list(alpha = 2L, beta = 3L)
   expect_identical(obj, exp)
 })
