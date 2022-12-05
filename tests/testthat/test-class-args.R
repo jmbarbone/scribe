8353b39f62d8ba938c8cfb851f34fecf2cb5832b
Jordan Mark Barbone <jbarbone@cogstate.com>
2022-12-05 16:37:04 -0500

    add tests

---
+++ tests/testthat/test-class-args.R
@@ -0,0 +1,18 @@
+test_that("$get_names() [#10]", {
+  x <- new_arg("-f", n = 1L)
+  expect_identical(x$get_name(), "f")
+  expect_identical(x$get_name(FALSE), "-f")
+
+  x <- new_arg(c("-f", "--foo"), n = 1L)
+  expect_identical(x$get_name(), "foo")
+  expect_identical(x$get_name(FALSE), "--foo")
+
+  x <- new_arg("foo", n = 1L)
+  expect_identical(x$get_name(), "foo")
+
+  x <- new_arg(c("...", "foo"), n = NA_integer_)
+  expect_identical(x$get_name(), "foo")
+
+  x <- new_arg("...", n = NA_integer_)
+  expect_identical(x$get_name(), "...")
+})
