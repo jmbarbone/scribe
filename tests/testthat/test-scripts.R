bfa1ea12bc702d293067fb39febb65901f8e883c
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-02-13 00:26:59 -0500

    #16 Create test-scripts.R

---
+++ tests/testthat/test-scripts.R
@@ -0,0 +1,31 @@
+path <- test_path("scripts/help.R")
+path <- normalizePath(path)
+Sys.chmod(path)
+
+test_that("help works", {
+  # lazy testing
+  obj <- system2(path, "--help", stdout = TRUE)[-3]
+  exp <- c(
+    "{scribe} command_args",
+    "",
+    "file :"[0],
+    "",
+    "USAGE",
+    "  help.R [--help | --version]",
+    "  help.R [--help] [--version] [-f, --foo [ARG]] [-b, --bar [ARG]] ",
+    "",
+    "ARGUMENTS",
+    "  --help          : prints this and quietly exits                   ",
+    "  --version       : prints the version of {scribe} and quietly exits",
+    "  -f, --foo [ARG] :                                                 ",
+    "  -b, --bar [ARG] :                                                 ",
+    NULL
+  )
+  expect_identical(obj, exp)
+})
+
+test_that("help works", {
+  obj <- system2(path, "--version", stdout = TRUE)
+  exp <- utils::capture.output(print_scribe_version())
+  expect_identical(obj, exp)
+})
