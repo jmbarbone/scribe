23b63a51748ca225b5600f4a0f6ac3be371300bf
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-04-30 23:01:27 -0400

    #54 test util

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -14,6 +14,7 @@ test_that("is_empty()", {
   expect_true(is_empty(NULL))
   expect_true(is_empty(integer()))
   expect_false(is_empty(is_empty))
+  expect_false(is_empty(new_arg()))
 })
 
 test_that("prints", {
