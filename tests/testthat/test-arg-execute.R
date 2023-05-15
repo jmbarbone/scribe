9202493439ea5bfd98c89cbbbd6d46ed6d079446
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-05-14 22:56:44 -0400

    #63 add more tests

--- tests/testthat/test-arg-execute.R
+++ tests/testthat/test-arg-execute.R
@@ -22,3 +22,35 @@ test_that("execute works", {
   ca$set_input("2")
   expect_message(ca$parse(), "The value is: 4")
 })
+
+test_that("execute formals work", {
+  arg0 <- new_arg(
+    "a",
+    default = 1,
+    execute = function() stopifnot(TRUE)
+  )
+
+  arg1 <- new_arg(
+    "b",
+    default = 1,
+    execute = function(x) stopifnot(is_arg(x))
+  )
+
+  arg3 <- new_arg(
+    "c",
+    default = 1,
+    execute = function(x, y, z) {
+      stopifnot(
+        is_arg(x),
+        is_command_args(y),
+        missing(z)
+      )
+    }
+  )
+
+  ca <- command_args()
+  ca$add_argument(arg0)
+  ca$add_argument(arg1)
+  ca$add_argument(arg3)
+  expect_error(ca$parse(), NA)
+})
