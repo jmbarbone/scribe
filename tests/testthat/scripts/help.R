d92871fd8223024cfa1f7752ed0a7eb58e40f12d
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-06-02 17:11:02 -0400

    use installed version of scribe for test scripts (#78)

--- tests/testthat/scripts/help.R
+++ tests/testthat/scripts/help.R
@@ -1,6 +1,10 @@
 #!/usr/bin/Rscript --vanilla
 
-library(scribe)
+if (!require(scribe, lib.loc = Sys.getenv("R_LIBS_SCRIBE", "~/R/scribe-library"))) {
+  warning("expected {scribe} to be installed at R_LIBS_SCRIBE")
+  library(scribe)
+}
+
 ca <- command_args()
 ca$add_argument("-f", "--foo")
 ca$add_argument("-b", "--bar")
